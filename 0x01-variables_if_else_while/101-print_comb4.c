1d1bcfa3b974689c402f552d886a02e6bf7cc138
Mimirow <rowlandmiracle73@gmail.com>
2022-09-11 09:47:37 -0700

    C

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,12 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
- {
+{
 	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
-		for(m = 49; m < 58; m++)
+		for (m = 49; m < 58; m++)
 		{
 			for (l = 50; l < 58; l++)
 			{
@@ -30,4 +30,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
- }
+}
