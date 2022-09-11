387df0f9c22f790c8375f868051346b918137256
Mimirow <rowlandmiracle73@gmail.com>
2022-09-11 16:51:28 -0700

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
+
