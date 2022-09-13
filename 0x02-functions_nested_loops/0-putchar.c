0b7e0e6f0ab617ea6bd6718ecb5bbb32bc13254c
Mimirow <rowlandmiracle73@gmail.com>
2022-09-13 13:13:16 -0700

    C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,18 @@
 
 /**
  * main - Entry point
- *
- * Description: prints _putchar using putchar prototype
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str[] = "putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t);
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return 0;
