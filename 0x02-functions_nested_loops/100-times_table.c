c6f4c4954529f8c0b6196ee7164cd0a16551ab22
Mimirow <rowlandmiracle73@gmail.com>
2022-09-13 18:05:52 -0700

    C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,7 @@ void print_times_table(int n)
 					if (j != 0)
 					{
 						_putchar(k + '0');
-				} else if (k , 10 && j != 0)
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -36,11 +36,12 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((k / 100) + '0');:
+					_putchar((k / 100) + '0'):
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
+			}
 		}
 }
