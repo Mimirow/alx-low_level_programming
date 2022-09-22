d7964401c8a50b5a6d2464657159137d9fa235d5
Mimirow <rowlandmiracle73@gmail.com>
2022-09-22 11:04:57 -0700

    Shell

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,7 +2,7 @@
 
 /**
  * rot13 - encodes a string using rot13
- * @s: string to encode 
+ * @s: string to encode
  *
  * Return: address of s
  */
@@ -12,7 +12,7 @@ char *rot13(char *s)
 	int i, j;
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	
+
 	for (i = 0; *(s + i); i++)
 	{
 		for (j = 0; j < 52; j++)
