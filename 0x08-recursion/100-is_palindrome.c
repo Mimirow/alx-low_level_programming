bb2e409b0703a0882568e745ecbc0efdd95c0c0d
Mimirow <rowlandmiracle73@gmail.com>
2022-09-28 12:23:17 -0700

    Shell

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _strlen_recursion - find length of string, recursively
  * @s: pointer to beginning of string
  * Return: int representing string length
  */
+
 int _strlen_recursion(char *s)
 {
 	if (*s)
