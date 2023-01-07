5a4c212adc867ed4778f7cd18ea46df749fb11ad
Mimirow <rowlandmiracle73@gmail.com>
2023-01-06 21:02:52 -0800

    Python

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,18 @@
+#include "./hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ *
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
