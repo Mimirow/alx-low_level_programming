0d3aff97eea5e782a2ebb702a2b33480dba76be1
Mimirow <rowlandmiracle73@gmail.com>
2023-01-06 21:05:52 -0800

    Python

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,18 +1,30 @@
-#include "./hash_tables.h"
+#include "hash_tables.h"
 
 /**
- * hash_table_print - Prints a hash table.
- * @ht: A pointer to the hash table to print.
- *
- * Description: Key/value pairs are printed in the order
- *              they appear in the array of the hash table.
+ * hash_table_print - prints a hash table.
+ * @ht: pointer to hash table.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned long int i;
-	unsigned char comma_flag = 0;
+	hash_node_t *temp_node;
+	unsigned int index, first = 0;
 
-	if (ht == NULL)
+	if (!ht)
 		return;
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		temp_node = ht->array[index];
+		while (temp_node)
+		{
+			if (first == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp_node->key, temp_node->value);
+			temp_node = temp_node->next;
+			first = 1;
+		}
+	}
+	printf("}\n");
+}
 
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,2 +1,1 @@
-
->>> ht['betty']
+>>> print(ht)
